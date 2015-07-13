9ac53f6dc9f965e21320b3699389204505e30852
vectorijk <jiangkai@gmail.com>
2015-07-13 04:14:50 -0700

    Add underline(experimental)

--- app/src/main/java/com/vectorijk/passwordindicator/MainActivity.java
+++ app/src/main/java/com/vectorijk/passwordindicator/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +13,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText First = (EditText) findViewById(R.id.first);
     }
 
     @Override
---
+++ zxcvbn/src/main/java/com/vectorijk/zxcvbn/PasswordEditText.java
@@ -0,0 +1,56 @@
+package com.vectorijk.zxcvbn;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.AppCompatEditText;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+
+/**
+ * Copyright (C) 2015 vectorijk <jiangkai@gmail.com>
+ * Licensed under the MIT License.
+ * http://opensource.org/licenses/MIT
+ */
+
+public class PasswordEditText extends AppCompatEditText {
+    TextPaint textPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    public PasswordEditText(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public PasswordEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+
+    }
+
+    private static int dp2px(Context context, float dp) {
+        Resources r = context.getResources();
+        float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics());
+        return Math.round(px);
+    }
+
+    @Override
+    protected void onDraw(@NonNull Canvas canvas) {
+        int startX = getScrollX();
+        int endX = getWidth();
+        int lineStartY = getScrollY() + getHeight() - getPaddingBottom();
+
+        getPaint().setAlpha(255);
+        paint.setColor(Color.RED);
+//        textPaint.setColor(Color.RED);
+//        canvas.drawText("draw Text", startX, endX, textPaint);
+        canvas.drawRect(startX, lineStartY, endX, lineStartY + dp2px(getContext(),1), paint);
+        super.onDraw(canvas);
+    }
+}
