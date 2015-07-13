309805797c60c807f3ce6317307becd7bf647f22
vectorijk <jiangkai@gmail.com>
2015-07-12 22:16:16 -0700

    Apply AppCompatEditText
    - Add Two math functions in scoring.java

--- zxcvbn/src/main/java/com/vectorijk/zxcvbn/PasswordText.java
+++ zxcvbn/src/main/java/com/vectorijk/zxcvbn/PasswordText.java
@@ -5,9 +5,15 @@ import android.support.v7.widget.AppCompatEditText;
 import android.util.AttributeSet;
 
 /**
- * Created by jiangkai on 7/12/15.
+ * Copyright (C) 2015 vectorijk <jiangkai@gmail.com>
+ * Licensed under the MIT License.
+ * http://opensource.org/licenses/MIT
  */
+
 public class PasswordText extends AppCompatEditText {
+    public PasswordText(Context context) {
+        super(context);
+    }
 
     public PasswordText(Context context, AttributeSet attrs) {
         super(context, attrs);
--- zxcvbn/src/main/java/com/vectorijk/zxcvbn/scoring.java
+++ zxcvbn/src/main/java/com/vectorijk/zxcvbn/scoring.java
@@ -1,7 +1,30 @@
 package com.vectorijk.zxcvbn;
 
 /**
- * Created by jiangkai on 7/12/15.
+ * Copyright (C) 2015 vectorijk <jiangkai@gmail.com>
+ * Licensed under the MIT License.
+ * http://opensource.org/licenses/MIT
  */
+
 public class scoring {
+    private int nCk(int n, int k) {
+        if (k > n) {
+            return 0;
+        }
+        if (k == 0) {
+            return 1;
+        }
+        int r = 1;
+        int d;
+        for (d = 1; d <= k; d++) {
+            r *= n;
+            r /= d;
+            n -= 1;
+        }
+        return r;
+    }
+
+    private double lg(int n) {
+        return Math.log(n) * 1.0 / Math.log(2);
+    }
 }
