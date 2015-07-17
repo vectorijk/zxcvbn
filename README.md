9d5efb43f802a2f8c488df3794395c6fc9462c07
vectorijk <jiangkai@gmail.com>
2015-07-16 19:05:47 -0700

    json files
    - add json file for dictionary
    - add gson for parse json file in build.gradle
    - Readme.md initialize

---
+++ README.md
@@ -0,0 +1,11 @@
+```
+ ____ ____ ____ ____ ____ ____
+||z |||x |||c |||v |||b |||n ||
+||__|||__|||__|||__|||__|||__||
+|/__\|/__\|/__\|/__\|/__\|/__\|
+```
+
+`zxcvbn-Android` is a password strength meter. `zxcvbn-Android` is base on the project from
+[Dropbox/zxcvbn](https://github.com/dropbox/zxcvbn)
+
+## Usage
--- zxcvbn/src/main/java/com/vectorijk/zxcvbn/Match.java
+++ zxcvbn/src/main/java/com/vectorijk/zxcvbn/Match.java
@@ -1,5 +1,7 @@
 package com.vectorijk.zxcvbn;
 
+import java.util.regex.Pattern;
+
 /**
  * Copyright (C) 2015 vectorijk <jiangkai@gmail.com>
  * Licensed under the MIT License.
@@ -10,4 +12,9 @@ public abstract class Match {
     public abstract double getEntropy();
 
     public abstract String getToke();
+
+    public static void findAll(String string, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+
+    }
 }
